396c438ee3d6e51b6a2de992d6ba0c6155173d18
tonykastaneda <tonykastaneda@gmail.com>
2021-08-16 15:42:04 -0700

    Broke Install Script

--- symlink.sh
+++ symlink.sh
@@ -1,5 +1,15 @@
-#!/usr/bin/env bash 
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-In -s ~/.dotfiles/.zsh ~/.zsh
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/.vim ~/.vim
+#!/bin/sh
+
+if [ -d "~/.zshrc" ] && [ ! -L "~/.zshrc" ] && echo "Directory ~/.zshrc exists." || echo "Error: Directory ~/.zshrc exists but point to $(readlink -f ~/.zshrc)."
+then
+  echo "rm -r ~/.zshrc"
+else
+  ech "ln -s ~/.dotfiles/.zshrc ~/.zshrc"
+
+
+
+# This is an inline comment in bash
+# ln -s ~/.dotfiles/.zshrc ~/.zshrc;
+# In -s ~/.dotfiles/.zsh ~/.zsh;
+# ln -s ~/.dotfiles/.vimrc ~/.vimrc;
+# ln -s ~/.dotfiles/.vim ~/.vim
