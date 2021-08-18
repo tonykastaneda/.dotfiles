9015473c8710607a4f4eea4e343aa911bb1a0aae
tonykastaneda <tonykastaneda@gmail.com>
2021-08-17 21:54:12 -0700

    reordered atomic - for archival puropse only

--- atomic.sh
+++ atomic.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
+git clone https://github.com/tonykastaneda/.dotfiles
+git clone https://github.com/tonykastaneda/RayCastScripts
 defaults write com.apple.Dock autohide-delay -float 0.0001; killall Dock
 defaults write NSGlobalDomain _HIHideMenuBar -bool true
-git clone https://github.com/tonykastaneda/.dotfiles
 cd ~/Documents
 mkdir "Web Projects"
 mkdir "Screenshots"
-git clone https://github.com/tonykastaneda/RayCastScripts
 cd ~/.dotfiles
 sh installer.sh
 sh brew.sh
