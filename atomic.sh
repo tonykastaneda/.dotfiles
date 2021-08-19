fdfdff7b62b20826d73de088ef8bcc4d97407805
tonykastaneda <tonykastaneda@gmail.com>
2021-08-18 20:51:58 -0700

    Update atomic.sh

--- atomic.sh
+++ atomic.sh
@@ -19,6 +19,16 @@ git clone https://github.com/tonykastaneda/RayCastScripts;
 mkdir "Web Projects";
 mkdir "Screenshots";
 vim +'PlugInstall --sync' +qa;
+# Running Apps In Dock Only
+defaults write com.apple.dock static-only -bool true; killall Dock;
+# Desktop Picture Setter
+osascript -e 'tell application "System Events" to tell every desktop to set picture to "~/.dotfiles/img/desktop.png"';
+# Auto Hide Dock
+defaults write com.apple.Dock autohide -bool TRUE; killall Dock;
+# Instant Dock Auto Hide PopUp
+defaults write com.apple.Dock autohide-delay -float 0.0001; killall Dock;
+# Auto Hide Menubar -- must close all apps ie terminal
+defaults write NSGlobalDomain _HIHideMenuBar -bool true; killall Finder;
 source ~/.zshrc
 
 
