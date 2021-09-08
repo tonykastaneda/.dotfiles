ef30bdfdce9c5adb7c70b2c2153b3db86c3bc8a8
tonykastaneda <tonykastaneda@gmail.com>
2021-09-08 09:30:52 -0700

    fixed yabai

--- yabainstaller.sh
+++ yabainstaller.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-ln -s ~/.dotfiles/YABAI/yabai ~/.config/yabai
-ln -s ~/.dotfiles/YABAI/skhd ~/.config/skhd
-ln -s ~/.dotfiles/YABAI/limelight  ~/.config/limelight
-ln -s ~/.dotfiles/YABAI/limelight-source/bin/limelight /usr/local/bin/limelight
+rm -r -f /usr/local/bin/limelight;
+rm -r -f ~/.config/yabai;
+rm -r -f ~/.config/skhd;
+rm -r -f ~/.config/limelight;
+
+
+ln -s ~/.dotfiles/YABAI/yabai ~/.config/yabai;
+ln -s ~/.dotfiles/YABAI/skhd ~/.config/skhd;
+ln -s ~/.dotfiles/YABAI/limelight  ~/.config/limelight;
+ln -s ~/.dotfiles/YABAI/limelight-source/bin/limelight /usr/local/bin/limelight;
 
